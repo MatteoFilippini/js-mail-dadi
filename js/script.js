289f9976db810069e14a48957e2bd509496c2a48
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-10 14:42:04 +0100

    consegne

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // console.log("ok");
+
+
+/*
+    MAIL
+    Crea un array di email;
+    Chiedi all’utente la sua email.
+    controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+ */
+
+/*
+  GIOCO DDEI DADI
+  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+  Stabilire il vincitore, in base a chi fa il punteggio più alto.
+  Stampare in console i tiri di dado e il risultato.
+*/
